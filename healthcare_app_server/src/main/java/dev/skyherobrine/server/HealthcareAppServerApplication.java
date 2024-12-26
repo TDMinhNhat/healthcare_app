9b693a0e8d6e2cb5ca4db99f7798ee20bf901d81
TDMinhNhat <tdminhnhat13092003@gmail.com>
2024-12-26 21:24:46 +0700

    Server: init server project

---
+++ healthcare_app_server/src/main/java/dev/skyherobrine/server/HealthcareAppServerApplication.java
@@ -0,0 +1,13 @@
+package dev.skyherobrine.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareAppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthcareAppServerApplication.class, args);
+    }
+
+}
