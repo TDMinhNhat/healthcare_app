5aa582c3192e06ea16ba85da99dde180f8294c78
TDMinhNhat <tdminhnhat13092003@gmail.com>
2024-12-30 23:04:41 +0700

    Api_gateway: config routes

---
+++ healthcare_app_server/api_gateway_service/src/main/java/dev/skyherobrine/service/configs/RoutesConfig.java
@@ -0,0 +1,17 @@
+package dev.skyherobrine.service.configs;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RoutesConfig {
+
+    @Bean
+    public RouteLocator configRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("authenticate_service", r -> r.path("/authenticate/**").uri("lb://authenticate_service/**"))
+                .build();
+    }
+}
