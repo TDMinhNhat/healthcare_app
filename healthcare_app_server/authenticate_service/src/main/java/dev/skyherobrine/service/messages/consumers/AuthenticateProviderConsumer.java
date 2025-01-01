e483bd8073b2cc5a0997ad3349db9f5094a7a980
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-01-01 21:07:51 +0700

    Authenticate: add listener insert authenticate_provider message

---
+++ healthcare_app_server/authenticate_service/src/main/java/dev/skyherobrine/service/messages/consumers/AuthenticateProviderConsumer.java
@@ -0,0 +1,26 @@
+package dev.skyherobrine.service.messages.consumers;
+
+import dev.skyherobrine.service.models.AuthenticateProvider;
+import dev.skyherobrine.service.repositories.AuthenticateProviderRepository;
+import dev.skyherobrine.service.utils.ObjectParser;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class AuthenticateProviderConsumer {
+
+    private final AuthenticateProviderRepository apr;
+
+    public AuthenticateProviderConsumer(AuthenticateProviderRepository apr) {
+        this.apr = apr;
+    }
+
+    @KafkaListener(topics = "insert_authenticate_provider", groupId = "insert_authenticate_provider_authenticate_admin")
+    public void insertAuthenticateProvider(String message) throws Exception {
+        log.info("Listen insert authenticate provider message");
+        AuthenticateProvider ap = ObjectParser.convertJsonToObject(message, AuthenticateProvider.class);
+        apr.save(ap);
+    }
+}
