0290024aa7f74a6a2b055e0f2fcceb4b2a4d5b93
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-01-06 00:21:21 +0700

    Website: add routes

--- healthcare_app_frontend/healthcare_app_web/src/App.tsx
+++ healthcare_app_frontend/healthcare_app_web/src/App.tsx
@@ -1,7 +1,19 @@
+import {BrowserRouter, Routes, Route} from "react-router";
+import HomePage from "./pages/HomePage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import RegisterPage from "./pages/RegisterPage.tsx";
+import AdminPage from "./pages/AdminPage.tsx";
 
 function App() {
     return (
-        <h1>Hello World!</h1>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={ <HomePage/> } />
+                <Route path="/login" element={ <LoginPage/> } />
+                <Route path="/register" element={ <RegisterPage/> } />
+                <Route path="/admin" element={ <AdminPage/> } />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
