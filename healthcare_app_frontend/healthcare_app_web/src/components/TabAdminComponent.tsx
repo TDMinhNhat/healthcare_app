feecdecb48d0902226eb2f051096b99f3121ef78
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-01-12 18:51:17 +0700

    Website: add direct to home in logo admin dashboard

--- healthcare_app_frontend/healthcare_app_web/src/components/TabAdminComponent.tsx
+++ healthcare_app_frontend/healthcare_app_web/src/components/TabAdminComponent.tsx
@@ -1,5 +1,6 @@
 import {Box, Stack, Tab, Tabs, Typography} from "@mui/material";
 import "../styles/tab-admin.scss"
+import {Link} from "react-router";
 function TabAdminComponent({ tab, setTab, tabsLanguage }:{ tab: string, setTab: void, tabsLanguage: object }) {
 
     const list = tabsLanguage.list;
@@ -8,16 +9,18 @@ function TabAdminComponent({ tab, setTab, tabsLanguage }:{ tab: string, setTab: 
         <Stack direction={"column"} className={"position-fixed top-0 start-0 d-flex flex-column align-items-center justify-content-between h-100 bg-white"}>
             <Stack direction={"column"}>
                 <Box className={"w-100 p-3"}>
-                    <img
-                        id={"logo"}
-                        src={"logo.png"}
-                        width={"200px"}
-                        height={"75px"}
-                    />
+                    <Link to={"/"}>
+                        <img
+                            id={"logo"}
+                            src={"logo.png"}
+                            width={"200px"}
+                            height={"75px"}
+                        />
+                    </Link>
                 </Box>
 
                 <Stack className={"w-100 d-flex flex-column align-items-start"}>
-                    { list.map((item: object, index: number) => {
+                    {list.map((item: object, index: number) => {
                         return (
                             <Box key={index} className={"d-flex flex-row justify-content-start align-items-center p-3 tab-item w-100"} onClick={() => setTab(item.value)}>
                                 <img src={item.image} width={"40px"} height={"40px"}/>
