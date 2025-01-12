301d65be4d406f0198b73cf48cc2bb3b8cee87ab
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-01-12 18:08:57 +0700

    Website: display user dashboard table || add tab admin

--- healthcare_app_frontend/healthcare_app_web/src/components/admin/UsersAdminComponent.tsx
+++ healthcare_app_frontend/healthcare_app_web/src/components/admin/UsersAdminComponent.tsx
@@ -1,26 +1,42 @@
-import {Box, Stack, Typography} from "@mui/material";
+import {
+    Box, Button,
+    Paper,
+    Stack,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography
+} from "@mui/material";
 import { useState, useEffect } from "react";
 import "../../styles/tab-users-admin.scss"
 
-function UsersAdminComponent() {
+function UsersAdminComponent({ tabUserContentLanguage }:{ tabUserContentLanguage: object }) {
+
+    const solveDeleteUser = (event, id) => {
+
+    }
+
     return (
         <Stack direction={"column"}>
             <Stack direction={"row"} className={"d-flex flex-row justify-content-evenly mt-3"}>
-                <Box className={"box-item d-flex flex-column align-items-center justify-content-center text-white p-3"} sx={{backgroundColor: "red"}}>
-                    <Typography variant={"h5"} className={"text-center"}>Số Người Dùng</Typography>
-                    <Typography variant={"h3"}>105</Typography>
+                <Box className={"box-item d-flex flex-column align-items-center justify-content-center text-white p-1"} sx={{backgroundColor: "red"}}>
+                    <Typography variant={"h6"} className={"text-center"}>{tabUserContentLanguage.total_users_system}</Typography>
+                    <Typography variant={"h4"}>105</Typography>
                 </Box>
-                <Box className={"box-item d-flex flex-column align-items-center justify-content-center text-white p-3"} sx={{backgroundColor: "blue"}}>
-                    <Typography variant={"h5"} className={"text-center"}>Số Bệnh Nhân</Typography>
-                    <Typography variant={"h3"}>80</Typography>
+                <Box className={"box-item d-flex flex-column align-items-center justify-content-center text-white p-1"} sx={{backgroundColor: "blue"}}>
+                    <Typography variant={"h6"} className={"text-center"}>{tabUserContentLanguage.total_users_patient}</Typography>
+                    <Typography variant={"h4"}>80</Typography>
                 </Box>
-                <Box className={"box-item d-flex flex-column align-items-center justify-content-center text-white p-3"} sx={{backgroundColor: "purple"}}>
-                    <Typography variant={"h5"} className={"text-center"}>Số Bác Sĩ</Typography>
-                    <Typography variant={"h3"}>20</Typography>
+                <Box className={"box-item d-flex flex-column align-items-center justify-content-center text-white p-1"} sx={{backgroundColor: "purple"}}>
+                    <Typography variant={"h6"} className={"text-center"}>{tabUserContentLanguage.total_users_doctor}</Typography>
+                    <Typography variant={"h4"}>20</Typography>
                 </Box>
-                <Box className={"box-item d-flex flex-column align-items-center justify-content-center text-white p-3"} sx={{backgroundColor: "green"}}>
-                    <Typography variant={"h5"} className={"text-center"}>Số Quản Lý</Typography>
-                    <Typography variant={"h3"}>4</Typography>
+                <Box className={"box-item d-flex flex-column align-items-center justify-content-center text-white p-1"} sx={{backgroundColor: "green"}}>
+                    <Typography variant={"h6"} className={"text-center"}>{tabUserContentLanguage.total_users_manager}</Typography>
+                    <Typography variant={"h4"}>4</Typography>
                 </Box>
             </Stack>
             <Stack direction={"column"}>
@@ -31,9 +47,40 @@ function UsersAdminComponent() {
 
                 </Box>
             </Stack>
-            <Box>
-
-            </Box>
+            <TableContainer id={"table"} component={Paper} className={"mt-5 w-100 d-flex flex-row"}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align={"center"}>{tabUserContentLanguage.table.id}</TableCell>
+                            <TableCell align={"center"}>{tabUserContentLanguage.table.userId}</TableCell>
+                            <TableCell align={"center"}>{tabUserContentLanguage.table.first_name}</TableCell>
+                            <TableCell align={"center"}>{tabUserContentLanguage.table.last_name}</TableCell>
+                            <TableCell align={"center"}>{tabUserContentLanguage.table.user_name}</TableCell>
+                            <TableCell align={"center"}>{tabUserContentLanguage.table.email}</TableCell>
+                            <TableCell align={"center"}>{tabUserContentLanguage.table.phone}</TableCell>
+                            <TableCell align={"center"}>{tabUserContentLanguage.table.role}</TableCell>
+                            <TableCell align={"center"}>{tabUserContentLanguage.table.status}</TableCell>
+                            <TableCell align={"center"}>{tabUserContentLanguage.table.action}</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell align={"center"}>1</TableCell>
+                            <TableCell align={"center"}>1</TableCell>
+                            <TableCell align={"center"}>John</TableCell>
+                            <TableCell align={"center"}>Doe</TableCell>
+                            <TableCell align={"center"}>john.doe</TableCell>
+                            <TableCell align={"center"}>1</TableCell>
+                            <TableCell align={"center"}>John</TableCell>
+                            <TableCell align={"center"}>Doe</TableCell>
+                            <TableCell align={"center"}>john.doe</TableCell>
+                            <TableCell align={"center"}>
+                                <Button variant={"contained"} color={"error"} onClick={(event) => solveDeleteUser(event, 1)}>{tabUserContentLanguage.table.btn_delete}</Button>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </Stack>
     )
 }
--- healthcare_app_frontend/healthcare_app_web/src/pages/AdminPage.tsx
+++ healthcare_app_frontend/healthcare_app_web/src/pages/AdminPage.tsx
@@ -1,23 +1,24 @@
 import {useState} from "react";
-import {Stack, Box} from "@mui/material";
+import {Box, Stack} from "@mui/material";
 import TabAdminComponent from "../components/TabAdminComponent.tsx"
 import DashboardAdminComponent from "../components/admin/DashboardAdminComponent.tsx";
 import UsersAdminComponent from "../components/admin/UsersAdminComponent.tsx";
 import AppointmentsAdminComponent from "../components/admin/AppointmentsAdminComponent.tsx";
 
-function AdminPage({ adminLanguage }:{ adminLanguage: object }) {
+function AdminPage({adminLanguage}: { adminLanguage: object }) {
 
     const [tab, setTab] = useState<string>("dashboard");
 
     return (
         <Stack direction={"row"} className="">
             <Box sx={{width: "200px", height: "100%"}}>
-                <TabAdminComponent tab={tab} setTab={setTab} tabsLanguage={adminLanguage.tabs} />
+                <TabAdminComponent tab={tab} setTab={setTab} tabsLanguage={adminLanguage.tabs}/>
             </Box>
             <Box className={"container-fluid ms-5 w-100 h-100"} sx={{backgroundColor: "#8D78FF68"}}>
-                { tab === "dashboard" && <DashboardAdminComponent /> }
-                { tab === "users" && <UsersAdminComponent /> }
-                { tab === "appointments" && <AppointmentsAdminComponent /> }
+                {tab === "dashboard" && <DashboardAdminComponent/>}
+                {tab === "users" &&
+                    <UsersAdminComponent tabUserContentLanguage={adminLanguage.tabs.tab_users_content}/>}
+                {tab === "appointments" && <AppointmentsAdminComponent/>}
             </Box>
         </Stack>
     )
