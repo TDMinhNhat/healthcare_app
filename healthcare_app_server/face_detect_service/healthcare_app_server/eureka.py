01d433d5a7690b3ed48efb5ac67aad3444803e48
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-01-20 13:28:18 +0700

    AI: remove ai_service

--- healthcare_app_server/face_detect_service/healthcare_app_server/eureka.py
+++ healthcare_app_server/face_detect_service/healthcare_app_server/eureka.py
@@ -3,7 +3,7 @@ from py_eureka_client.eureka_client import EurekaClient
 
 def register_service():
     eureka_client = EurekaClient(eureka_server="http://localhost:8761/eureka/",
-                                 app_name="ai_service",
+                                 app_name="face_detect_service",
                                  instance_port=8000)
     asyncio.run(eureka_client.start())
     return eureka_client
--- healthcare_app_server/face_detect_service/healthcare_app_server/settings.py
+++ healthcare_app_server/face_detect_service/healthcare_app_server/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'healthcare_app_server'
 ]
 
 MIDDLEWARE = [
