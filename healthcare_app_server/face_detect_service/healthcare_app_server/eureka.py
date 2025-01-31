93266144f25c604cb96f837e612e78399fc0998d
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-01-31 11:00:58 +0700

    remove 2 modules

--- healthcare_app_server/face_detect_service/healthcare_app_server/eureka.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from py_eureka_client.eureka_client import EurekaClient
-
-def register_service():
-    eureka_client = EurekaClient(eureka_server="http://localhost:8761/eureka/",
-                                 app_name="face_detect_service",
-                                 instance_port=8000)
-    asyncio.run(eureka_client.start())
-    return eureka_client
