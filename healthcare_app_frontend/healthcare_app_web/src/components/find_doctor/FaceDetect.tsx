66f2ea9215ce64fa9b829ab7bb6b22f596ef5836
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-02-05 00:16:48 +0700

    Website: add components for FindDoctor

---
+++ healthcare_app_frontend/healthcare_app_web/src/components/find_doctor/FaceDetect.tsx
@@ -0,0 +1,6 @@
+
+export default function FaceDetect() {
+    return (
+        <h1>Face Detect</h1>
+    )
+}
---
+++ healthcare_app_frontend/healthcare_app_web/src/components/find_doctor/GPSMap.tsx
@@ -0,0 +1,6 @@
+
+export default function GPSMap() {
+    return (
+        <h1>GPS Map</h1>
+    )
+}
