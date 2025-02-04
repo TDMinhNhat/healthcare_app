7f30ab439d9059c8d9121da88c18e8dbaf34c82c
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-02-04 12:57:02 +0700

    Image_detect: add user model

--- healthcare_app_server/image_detect_service/healthcare_app_server/models.py
+++ healthcare_app_server/image_detect_service/healthcare_app_server/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+class User(models.Model):
+    class Meta:
+        db_table = "users"
+
+    id = models.BigAutoField(primary_key = True)
+    user_id = models.CharField(max_length = 30, null = False, blank = False, unique = True)
+    face_image = models.CharField(max_length = 100, null = False, blank = False)
+
 class TypeDetect(models.Model):
     class Meta:
         db_table = "type_detects"
@@ -14,9 +22,8 @@ class HistoryDetect(models.Model):
         db_table = "history_detects"
 
     id = models.BigAutoField(primary_key = True)
-    user_id = models.CharField(max_length = 25, null = False, blank = False, unique = True)
+    user = models.ForeignKey(User, on_delete = models.SET_NULL, null = True, blank = True, default = None)
     type_detect_id = models.ForeignKey(TypeDetect, on_delete = models.CASCADE, null = False, blank = False)
     image_detect_id = models.CharField(max_length = 100, null = False, blank = False, unique = True)
     success = models.BooleanField(null = False, blank = False, default = False)
     created_at = models.DateTimeField(auto_now_add = True, null = False, blank = False)
-
