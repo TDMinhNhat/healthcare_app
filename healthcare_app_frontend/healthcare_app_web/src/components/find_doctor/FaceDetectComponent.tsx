7bd26bc8eb0e19608a4dd1afcd76a25378f49602
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-02-13 09:00:21 +0700

    Website: solving not use camera when user existed

--- healthcare_app_frontend/healthcare_app_web/src/components/find_doctor/FaceDetectComponent.tsx
+++ healthcare_app_frontend/healthcare_app_web/src/components/find_doctor/FaceDetectComponent.tsx
@@ -24,50 +24,53 @@ export default function FaceDetectComponent({language, setStep}: { language: obj
     }
 
     useEffect(() => {
-        intervalId.current = setInterval(async () => {
-            const hasPermission = await checkCameraPermission();
-            if (!hasPermission) {
-                console.log("Camera is required for authentication");
-                return;
-            }
-
-            const base64Image = webcamRef.current.getScreenshot();
-            const byteCharacters = atob(base64Image.split(",")[1]);
-            const byteNumbers = new Array(byteCharacters.length).fill().map((_, i) => byteCharacters.charCodeAt(i));
-            const byteArray = new Uint8Array(byteNumbers);
-
-            for (let i = 0; i < byteCharacters.length; i++) {
-                byteArray[i] = byteCharacters.charCodeAt(i);
-            }
-
-            const blob = new Blob([byteArray], {type: "image/jpeg"});
-
-            const formData = new FormData();
-            formData.append("file", blob, "image.jpg");
-
-            const result = await axios.post("http://localhost:8081/image_detect/face/auth", formData, {
-                headers: {
-                    "Content-Type": "multipart/form-data"
+        if(user == null) {
+            intervalId.current = setInterval(async () => {
+                const hasPermission = await checkCameraPermission();
+                if (!hasPermission) {
+                    console.log("Camera is required for authentication");
+                    return;
                 }
-            }).then(response => response.data).catch(error => console.log("Server return an error: ", error))
-            console.log(result)
-            if (result.code === 200) {
-                if (result.message !== "New User") {
-                    stopCamera();
-                    sessionStorage.setItem("user", result.data);
-                    dispatch(setUser(result.data))
-                    setStep("gps");
+
+                const base64Image = webcamRef.current.getScreenshot();
+                const byteCharacters = atob(base64Image.split(",")[1]);
+                const byteNumbers = new Array(byteCharacters.length).fill().map((_, i) => byteCharacters.charCodeAt(i));
+                const byteArray = new Uint8Array(byteNumbers);
+
+                for (let i = 0; i < byteCharacters.length; i++) {
+                    byteArray[i] = byteCharacters.charCodeAt(i);
+                }
+
+                const blob = new Blob([byteArray], {type: "image/jpeg"});
+
+                const formData = new FormData();
+                formData.append("file", blob, "image.jpg");
+
+                const result = await axios.post("http://localhost:8081/image_detect/face/auth", formData, {
+                    headers: {
+                        "Content-Type": "multipart/form-data"
+                    }
+                }).then(response => response.data).catch(error => console.log("Server return an error: ", error))
+                if (result.code === 200) {
+                    if (result.message !== "New User") {
+                        stopCamera();
+                        sessionStorage.setItem("user", result.data);
+                        dispatch(setUser(result.data))
+                        setStep("gps");
+                    } else {
+                        sessionStorage.setItem("face_detect", result.data);
+                        setStep("info");
+                    }
                 } else {
-                    sessionStorage.setItem("face_detect", result.data);
-                    setStep("info");
+                    console.log(result);
                 }
-            } else {
-                console.log(result);
-            }
-        }, 2000)
+            }, 2000)
 
-        return () => {
-            clearInterval(intervalId.current)
+            return () => {
+                clearInterval(intervalId.current)
+            }
+        } else {
+            setStep("gps");
         }
 
     }, [])
