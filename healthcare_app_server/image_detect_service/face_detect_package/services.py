236a4a70dd2919951af2a807b67449e91d6838c6
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-02-19 13:33:46 +0700

    Image_detect: add API authenticate by face

--- healthcare_app_server/image_detect_service/face_detect_package/services.py
+++ healthcare_app_server/image_detect_service/face_detect_package/services.py
@@ -19,6 +19,44 @@ class FaceDetectService:
     def __init__(self, face_image):
         self.face_image = face_image
 
+    def detect_face_register(self) -> str:
+        image_array = np.asarray(bytearray(self.face_image), dtype=np.uint8)
+        image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+
+        if image is None:
+            raise RuntimeError("Image can't be decoded")
+
+        (h, w) = image.shape[:2]
+
+        blob = cv2.dnn.blobFromImage(image, scalefactor = 1.0, size = (300, 300), mean = (104.0, 177.0, 123.0))
+        self.net.setInput(blob)
+        detections = self.net.forward()
+
+        if len(detections) == 0:
+            print("No face detected")
+            return "No Detect"
+        elif len(detections) > 1:
+            print("Must be 1 face in image")
+            return "Multiple Face"
+
+        i = np.argmax(detections[0, 0, :, 2])
+        confidence = detections[0, 0, i, 2]
+
+        if confidence >= 0.95:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            roi = image[startY:endY, startX:endX]
+            faceBlob = cv2.dnn.blobFromImage(roi, 1/255, (96, 96), (0, 0, 0), swapRB = True, crop = True)
+
+            self.descriptor_model.setInput(faceBlob)
+            vector = self.descriptor_model.forward().flatten()
+            vector_str = ",".join(map(str, vector.tolist()))
+            return vector_str
+
+        return "Can't detect"
+
+
     def detect_face(self) -> str:
         image_array = np.asarray(bytearray(self.face_image), dtype=np.uint8)
         image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
--- healthcare_app_server/image_detect_service/face_detect_package/urls.py
+++ healthcare_app_server/image_detect_service/face_detect_package/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from face_detect_package import views
 
 urlpatterns = [
-    path('auth', views.send_image)
+    path('auth', views.authenticate_face),
+    path('register', views.register_face)
 ]
