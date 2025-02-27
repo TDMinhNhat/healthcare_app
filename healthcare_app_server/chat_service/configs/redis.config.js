553037952f0b76f47b1e9c9ede38317ea3d291bf
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-02-27 17:19:21 +0700

    Chat: add redis database

---
+++ healthcare_app_server/chat_service/configs/redis.config.js
@@ -0,0 +1,9 @@
+const { createClient } = require("redis");
+
+const client = createClient();
+
+(async () => {
+    await client.connect()
+})().catch(error => console.log(error))
+
+module.exports = client;
