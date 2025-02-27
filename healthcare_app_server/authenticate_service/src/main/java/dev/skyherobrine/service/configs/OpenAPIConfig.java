b7f9b348cab6e4f9e4c2be4cac32be51540eec14
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-02-27 14:49:51 +0700

    configure swagger document

---
+++ healthcare_app_server/authenticate_service/src/main/java/dev/skyherobrine/service/configs/OpenAPIConfig.java
@@ -0,0 +1,20 @@
+package dev.skyherobrine.service.configs;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@OpenAPIDefinition
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI userOpenAPI() {
+        return new OpenAPI().servers(List.of(new Server().url("http://localhost:9000"))).info(new Info().title("Authenticate API Service").version("1.0.0"));
+    }
+}
