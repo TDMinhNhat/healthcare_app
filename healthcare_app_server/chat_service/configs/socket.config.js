a68f3013a1010477a7bfccd435c8bebe86735e02
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-03-01 07:22:46 +0700

    Chat: add socket chat message

--- healthcare_app_server/chat_service/configs/socket.config.js
+++ healthcare_app_server/chat_service/configs/socket.config.js
@@ -1,6 +1,7 @@
 const {Server} = require("socket.io");
 const chat = require("../sockets/chat.socket");
 const call = require("../sockets/call.socket");
+const redisClient = require("../configs/redis.config");
 
 const run = (server) => {
     const io = new Server(server, {
@@ -9,6 +10,11 @@ const run = (server) => {
 
     io.on("connection", async (socket) => {
         console.log("A user has connected to the server");
+
+        socket.on("send_user_connect", (data) => {
+            const { userId } = data;
+            redisClient.set(userId, socket.id);
+        })
 
         chat(socket);
         call(socket);
