a1567c5d5052e72b6d1cf1e8edce8fa18a92319a
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-03-14 22:07:44 +0700

    Mobile: fix forgot lambda function

--- healthcare_app_frontend/healthcare_app_mobile/services/authenticate/authenticateService.ts
+++ healthcare_app_frontend/healthcare_app_mobile/services/authenticate/authenticateService.ts
@@ -10,7 +10,7 @@ const checkLogin = async (email: string, password: string) => {
     });
 }
 
-const register = async (firstName: string, lastName: string, sex: boolean, dob: Date, phone: string, username: string, email: string, password: string){
+const register = async (firstName: string, lastName: string, sex: boolean, dob: Date, phone: string, username: string, email: string, password: string) => {
     return await axiosConfig.post(`${prefix}/register`, {
         "firstName": firstName,
         "lastName": lastName,
