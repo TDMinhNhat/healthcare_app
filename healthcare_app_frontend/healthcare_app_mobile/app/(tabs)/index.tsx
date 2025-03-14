2e55c29b0321587c805f578c2c1d78ccc695c4e2
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-03-14 11:19:32 +0700

    Mobile: build login page

--- healthcare_app_frontend/healthcare_app_mobile/app/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { SafeAreaView, Text, StatusBar } from 'react-native';
-
-export default function HomeScreen() {
-  return (
-    <SafeAreaView style={{marginTop: StatusBar.currentHeight }}>
-        <Text>Hello World!</Text>
-    </SafeAreaView>
-  );
-}
---
+++ healthcare_app_frontend/healthcare_app_mobile/app/forgot_password.tsx
@@ -0,0 +1,9 @@
+import {SafeAreaView, Text} from "react-native";
+
+export default function ForgotPassword() {
+    return (
+        <SafeAreaView>
+            <Text>Forgot Password Component</Text>
+        </SafeAreaView>
+    )
+}
