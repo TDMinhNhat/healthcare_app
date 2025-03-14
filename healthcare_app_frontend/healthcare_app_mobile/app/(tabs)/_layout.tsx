7a93f6f67057a6ed2db70fa072ebd8f812b200f9
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-03-15 01:56:31 +0700

    Mobile: turn off the header shown

--- healthcare_app_frontend/healthcare_app_mobile/app/(tabs)/_layout.tsx
+++ healthcare_app_frontend/healthcare_app_mobile/app/(tabs)/_layout.tsx
@@ -3,9 +3,15 @@ import {Tabs} from "expo-router";
 export default function TabLayout() {
     return (
         <Tabs>
-            <Tabs.Screen name={"dashboard"} />
-            <Tabs.Screen name={"appointments"} />
-            <Tabs.Screen name={"users"} />
+            <Tabs.Screen name={"dashboard"} options={{
+                headerShown: false
+            }} />
+            <Tabs.Screen name={"appointments"} options={{
+                headerShown: false
+            }}/>
+            <Tabs.Screen name={"users"} options={{
+                headerShown: false
+            }} />
         </Tabs>
     )
 }
