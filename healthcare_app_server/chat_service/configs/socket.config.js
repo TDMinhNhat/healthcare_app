d268552c80d4eb8ca9559f2fbf778991194f2e9c
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-03-19 20:41:02 +0700

    Chat: solving waiting || chat room socket

--- healthcare_app_server/chat_service/configs/socket.config.js
+++ healthcare_app_server/chat_service/configs/socket.config.js
@@ -1,7 +1,4 @@
 const {Server} = require("socket.io");
-const chat = require("../sockets/chat.socket");
-const call = require("../sockets/call.socket");
-const redisClient = require("../configs/redis.config");
 
 const run = (server) => {
     const io = new Server(server, {
@@ -9,16 +6,36 @@ const run = (server) => {
     });
 
     io.on("connection", async (socket) => {
-        console.log("A user has connected to the server");
+        doctor(socket);
+        patient(socket);
+    })
 
-        socket.on("send_user_connect", (data) => {
-            const { userId } = data;
-            redisClient.set(userId, socket.id);
+    const doctor = (socket) => {
+        // Doctor joining the room
+        socket.on("doctorJoinRoom", (data) => {
+            socket.join("clinic: " + data.scheduleId);
+            socket.join("waiting: " + data.scheduleId);
         })
 
-        chat(socket);
-        call(socket);
-    })
+        //The doctor accepts a patient join the call room
+        socket.on("acceptPatient", (data) => {
+            socket.to("waiting: " + data.scheduleId).emit("patientAccepted", data);
+        })
+    }
+
+    const patient = (socket) => {
+        //The patient joining the waiting room
+        socket.on("joinWaitingQueue", (data) => {
+            socket.join("waiting: " + data.scheduleId);
+            socket.to("waiting: " + data.scheduleId).emit("patientQueueUpdate", data);
+        })
+
+        //The patient joining the call room
+        socket.on("patientJoinRoom", (data) => {
+            socket.leave("waiting: " + data.scheduleId);
+            socket.join("clinic: " + data.scheduleId);
+        })
+    }
 }
 
 module.exports = run;
