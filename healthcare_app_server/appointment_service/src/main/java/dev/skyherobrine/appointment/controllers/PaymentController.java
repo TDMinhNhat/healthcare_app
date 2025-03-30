8640b13110a0eed3fb0d343f6b4a457b9433cfe0
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-03-30 08:27:01 +0700

    Appointment: add api for getting payment

--- healthcare_app_server/appointment_service/src/main/java/dev/skyherobrine/appointment/controllers/PaymentController.java
+++ healthcare_app_server/appointment_service/src/main/java/dev/skyherobrine/appointment/controllers/PaymentController.java
@@ -7,10 +7,7 @@ import dev.skyherobrine.appointment.services.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/appointment/api/v1/payment")
@@ -18,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentController {
 
     private final PaymentService paymentService;
+    private final PaymentRepository paymentRepository;
 
-    public PaymentController(PaymentService paymentService) {
+    public PaymentController(PaymentService paymentService, PaymentRepository paymentRepository) {
         this.paymentService = paymentService;
+        this.paymentRepository = paymentRepository;
     }
 
     @PostMapping
@@ -37,6 +36,46 @@ public class PaymentController {
             log.error(e.getMessage());
             return ResponseEntity.ok(new Response(
                     HttpStatus.OK.value(),
+                    "The api thrown an error",
+                    e.getMessage()
+            ));
+        }
+    }
+
+    @GetMapping("/patient")
+    public ResponseEntity<Response> getAllPaymentByUserId(@RequestParam("patientId") String patientId) {
+        try {
+            log.info("Payment: Call the api get all payments by patient id");
+            return ResponseEntity.ok(new Response(
+                    HttpStatus.OK.value(),
+                    "Get all payments by patient id successfully!",
+                    paymentRepository.findByBookAppointment_PatientId(patientId)
+            ));
+        } catch (Exception e) {
+            log.error("Payment: The api thrown an error");
+            log.error(e.getMessage());
+            return ResponseEntity.ok(new Response(
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    "The api thrown an error",
+                    e.getMessage()
+            ));
+        }
+    }
+
+    @GetMapping("/book_appointment")
+    public ResponseEntity<Response> getPaymentByBookAppointment(@RequestParam("bookAppointmentId") String bookAppointmentId) {
+        try {
+            log.info("Payment: Call the api get payment by book appointment id");
+            return ResponseEntity.ok(new Response(
+                    HttpStatus.OK.value(),
+                    "Get payment by book appointment id successfully!",
+                    paymentRepository.findByBookAppointment_Id(Long.parseLong(bookAppointmentId)).orElse(null)
+            ));
+        } catch (Exception e) {
+            log.error("Payment: The api thrown an error");
+            log.error(e.getMessage());
+            return ResponseEntity.ok(new Response(
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(),
                     "The api thrown an error",
                     e.getMessage()
             ));
--- healthcare_app_server/appointment_service/src/main/java/dev/skyherobrine/appointment/repositories/mongodb/PaymentRepository.java
+++ healthcare_app_server/appointment_service/src/main/java/dev/skyherobrine/appointment/repositories/mongodb/PaymentRepository.java
@@ -4,9 +4,14 @@ import dev.skyherobrine.appointment.models.mongodb.Payment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PaymentRepository extends MongoRepository<Payment,Long> {
+    Optional<Payment> findByBookAppointment_Id(Long id);
+
     Optional<Payment> findTopByOrderByIdDesc();
+
+    List<Payment> findByBookAppointment_PatientId(String patientId);
 }
