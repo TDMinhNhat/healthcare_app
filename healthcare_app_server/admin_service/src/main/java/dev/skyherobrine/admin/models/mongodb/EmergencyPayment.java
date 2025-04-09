8ec5791e9c84159005d21dd4cbf31b0d93722e80
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-04-10 00:13:00 +0700

    Admin: add Emergency models

--- healthcare_app_server/admin_service/src/main/java/dev/skyherobrine/admin/models/mongodb/EmergencyPayment.java
+++ healthcare_app_server/admin_service/src/main/java/dev/skyherobrine/admin/models/mongodb/EmergencyPayment.java
@@ -1,24 +1,22 @@
 package dev.skyherobrine.admin.models.mongodb;
 
-import dev.skyherobrine.admin.models.mariadb.Patient;
+import dev.skyherobrine.admin.models.mongodb.Emergency;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.time.LocalDateTime;
 
 @Document(collection = "emergency_payments")
 @Getter @Setter
 @NoArgsConstructor
 public class EmergencyPayment extends Payment{
 
-    private LocalDateTime emergencyTime;
-    private Patient patient;
+    private Emergency emergency;
+    private String patientId;
 
-    public EmergencyPayment(Long id, Double price, String content, Patient patient) {
+    public EmergencyPayment(Long id, Double price, String content, String patientId, Emergency emergency) {
         super(id, price, content);
-        this.patient = patient;
-        this.emergencyTime = LocalDateTime.now();
+        this.patientId = patientId;
+        this.emergency = emergency;
     }
 }
