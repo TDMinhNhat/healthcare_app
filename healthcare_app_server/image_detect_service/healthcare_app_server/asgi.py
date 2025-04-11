03ba3eb151b2e79ae2e50566583ad71aa9dc6d82
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-04-11 21:32:04 +0700

    ImageDetect: setup socket server

--- healthcare_app_server/image_detect_service/healthcare_app_server/asgi.py
+++ healthcare_app_server/image_detect_service/healthcare_app_server/asgi.py
@@ -8,9 +8,29 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
-from django.core.asgi import get_asgi_application
+import socketio
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healthcare_app_server.settings')
 
-application = get_asgi_application()
+import django
+django.setup()
+
+from django.core.asgi import get_asgi_application
+import healthcare_app_server.eureka
+from healthcare_app_server.kafka_consumer import KafkaConsumer
+sio = socketio.AsyncServer(async_mode = 'asgi', cors_allowed_origins = '*', always_connect=False)
+django_asgi_application = get_asgi_application()
+
+application = socketio.ASGIApp(sio, django_asgi_application, socketio_path="/image_detect/socket")
+KafkaConsumer().start()
+
+@sio.event
+async def connect(sid, environ):
+    print("Client connected: ", sid)
+    pass
+
+@sio.event
+async def disconnect(sid):
+    print("Client disconnected: ", sid)
+    pass
+
