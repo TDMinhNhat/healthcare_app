4965ebd1fdfaa22dbf00e3040654e20f35745973
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-04-11 23:08:24 +0700

    Mobile: add socket for emergency

--- healthcare_app_frontend/healthcare_app_mobile/app/emergency.tsx
+++ healthcare_app_frontend/healthcare_app_mobile/app/emergency.tsx
@@ -16,6 +16,8 @@ import * as ImagePicker from "expo-image-picker";
 import { MaterialIcons, Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 import { detectFace } from "../services/image_detect/detect_service";
+import { io, Socket } from "socket.io-client";
+import * as FileSystem from "expo-file-system";
 
 export default function Emergency() {
   const [permission, requestPermission] = useCameraPermissions(); // State quản lý quyền truy cập camera
@@ -30,7 +32,17 @@ export default function Emergency() {
   >(null); // State quản lý quyền truy cập thư viện ảnh
 
   const cameraRef = useRef<CameraView>(null); // Tham chiếu đến component camera
+  const getIntervalNumber = useRef(0);
   const router = useRouter(); // Hook điều hướng
+  const [socket, setSocket] = useState<Socket>(
+    io(`ws://${process.env.EXPO_PUBLIC_HOST_ID}:8081`, {
+      path: "/image_detect/socket",
+      transports: ["websocket", "polling"],
+      reconnection: true,
+      reconnectionAttempts: 10,
+      autoConnect: false,
+    })
+  );
 
   // Yêu cầu quyền truy cập thư viện ảnh khi component được render
   useEffect(() => {
@@ -48,6 +60,35 @@ export default function Emergency() {
         );
       }
     })();
+
+    socket.connect();
+    socket.on("connect", () => {
+      socket.on("emergency_detect_response", (data) => { 
+        
+      })
+    });
+
+    getIntervalNumber.current = setInterval(() => {
+      (async () => {
+        const photo = await cameraRef.current.takePictureAsync({
+          quality: 0.8, // Chất lượng ảnh 80%
+        });
+        
+        const fileURI = photo.uri;
+        const base64Image = await FileSystem.readAsStringAsync(fileURI, {
+          encoding: FileSystem.EncodingType.Base64,
+        });
+        socket.emit("emergency_detect_request", {
+          image: base64Image,
+          fileName: "face.jpg"
+        });
+      })()
+    }, 2000);
+
+    return () => {
+      socket.disconnect();
+      clearInterval(getIntervalNumber.current);
+    }
   }, []);
 
   // Hàm được gọi khi camera sẵn sàng sử dụng
