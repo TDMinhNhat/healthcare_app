4a4d57fb08ede26210e3e3e962de52c007f6367a
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-05-05 10:14:53 +0700

    finish deploy backend to the cloud server

--- healthcare_app_server/api_gateway_service/src/main/java/dev/skyherobrine/api_gateway/configs/RoutesConfig.java
+++ healthcare_app_server/api_gateway_service/src/main/java/dev/skyherobrine/api_gateway/configs/RoutesConfig.java
@@ -7,20 +7,22 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.URI;
+
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "API Gateway Service", version = "1.0.0", description = "Documentation API Gateway Service v1.0.0"))
 public class RoutesConfig {
 
     @Bean
-    public RouteLocator configRouteLocator(RouteLocatorBuilder builder) {
+    public RouteLocator configRouteLocator(RouteLocatorBuilder builder) throws Exception {
         return builder.routes()
-                .route("authenticate_service", r -> r.path("/authenticate/**").uri("http://authenticate_service:9000"))
-                .route("admin_service", r -> r.path("/admin/**").uri("http://admin_service:10000"))
-                .route("appointment_service", r -> r.path("/appointment/**").uri("http://appointment_service:11000"))
-                .route("image_detect_service", r -> r.path("/image_detect/**").uri("http://image_detect_service:8000"))
-                .route("gps_service", r -> r.path("/gps/**").uri("ws://gps_service:3000"))
-                .route("chat_service", r -> r.path("/chat/**").uri("ws://chat_service:4000"))
-                .route("chatbot_service", r -> r.path("/chatbot/**").uri("http://chatbot_service:14000"))
+                .route("authenticate_service", r -> r.path("/authenticate/**").uri("http://authenticate:9000"))
+                .route("admin_service", r -> r.path("/admin/**").uri("http://admin:10000"))
+                .route("appointment_service", r -> r.path("/appointment/**").uri("http://appointment:11000"))
+                .route("image_detect_service", r -> r.path("/image_detect/**").uri("http://imagedetect:8000"))
+                .route("gps_service", r -> r.path("/gps/**").uri("ws://gps:3000"))
+                .route("chat_service", r -> r.path("/chat/**").uri("ws://chat:4000"))
+                .route("chatbot_service", r -> r.path("/chatbot/**").uri("http://chatbot:14000"))
                 .build();
     }
 }
