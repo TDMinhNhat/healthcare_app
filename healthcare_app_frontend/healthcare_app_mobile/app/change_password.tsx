90823ff112722135d81e6f0670a90d52b71428f5
dangquang <dangquang@duck.com>
2025-05-10 13:30:13 +0700

    mobile: update final v3

--- healthcare_app_frontend/healthcare_app_mobile/app/change_password.tsx
+++ healthcare_app_frontend/healthcare_app_mobile/app/change_password.tsx
@@ -20,7 +20,8 @@ import { router } from "expo-router";
 const ChangePasswordSchema = Yup.object().shape({
   oldPassword: Yup.string().required("Vui lòng nhập mật khẩu cũ"),
   newPassword: Yup.string()
-    .min(6, "Mật khẩu mới phải có ít nhất 6 ký tự")
+    .min(8, "Mật khẩu mới phải có ít nhất 68 ký tự")
+    .max(50, "Mật khẩu không được vượt quá 50 ký tự")
     .required("Vui lòng nhập mật khẩu mới"),
   confirmPassword: Yup.string()
     .oneOf([Yup.ref("newPassword"), null], "Mật khẩu xác nhận không khớp")
--- healthcare_app_frontend/healthcare_app_mobile/app/index.tsx
+++ healthcare_app_frontend/healthcare_app_mobile/app/index.tsx
@@ -24,7 +24,8 @@ import * as Yup from "yup";
 const LoginSchema = Yup.object().shape({
   email: Yup.string().email("Email không hợp lệ").required("Email là bắt buộc"),
   password: Yup.string()
-    .min(6, "Mật khẩu phải có ít nhất 6 ký tự")
+    .min(8, "Mật khẩu phải có ít nhất 8 ký tự")
+    .max(50, "Mật khẩu không được vượt quá 50 ký tự")
     .required("Mật khẩu là bắt buộc"),
 });
 
