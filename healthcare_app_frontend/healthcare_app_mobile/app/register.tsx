a5406bb35365f3ae59bf7721ef189b5503e0faad
dangquang <dangquang@duck.com>
2025-05-15 07:48:47 +0700

    mobile: limit pass length 20

--- healthcare_app_frontend/healthcare_app_mobile/app/register.tsx
+++ healthcare_app_frontend/healthcare_app_mobile/app/register.tsx
@@ -43,7 +43,7 @@ const validationSchema = Yup.object({
     .required("Số điện thoại là bắt buộc"),
   password: Yup.string()
     .min(8, "Mật khẩu phải có ít nhất 8 ký tự")
-    .max(50, "Mật khẩu không được vượt quá 50 ký tự")
+    .max(20, "Mật khẩu không được vượt quá 20 ký tự")
     .required("Mật khẩu là bắt buộc"),
 });
 
