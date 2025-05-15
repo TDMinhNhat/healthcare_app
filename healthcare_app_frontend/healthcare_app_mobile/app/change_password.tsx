804b02290f32206868c1f92654278cf8e68df6cc
dangquang <dangquang@duck.com>
2025-05-15 07:51:17 +0700

    mobile: change pass 20

--- healthcare_app_frontend/healthcare_app_mobile/app/change_password.tsx
+++ healthcare_app_frontend/healthcare_app_mobile/app/change_password.tsx
@@ -21,7 +21,7 @@ const ChangePasswordSchema = Yup.object().shape({
   oldPassword: Yup.string().required("Vui lòng nhập mật khẩu cũ"),
   newPassword: Yup.string()
     .min(8, "Mật khẩu mới phải có ít nhất 68 ký tự")
-    .max(50, "Mật khẩu không được vượt quá 50 ký tự")
+    .max(20, "Mật khẩu không được vượt quá 20 ký tự")
     .required("Vui lòng nhập mật khẩu mới"),
   confirmPassword: Yup.string()
     .oneOf([Yup.ref("newPassword"), null], "Mật khẩu xác nhận không khớp")
